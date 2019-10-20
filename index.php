cb4c089425808edbaa6e0e17d9d3eac4974b9dee
Jose Felix Cabrera <josefelixcv@gmail.com>
2019-10-20 15:53:38 -0400

    Iniciating

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    if (isset($_GET['hub_mode']) && isset($_GET['hub_challenge']) && isset($_GET['hub_verify_token'])) {
+        if ($_GET['hub_verify_token'] == 'my_verify_token')
+            echo $_GET['hub_challenge'];
+    }
+
+    http_response_code(200);
+?>
